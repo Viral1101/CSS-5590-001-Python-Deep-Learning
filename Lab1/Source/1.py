64f379ae812058e48cb42b930b8823d3e83967a1
Viral1101 <Viral1101@users.noreply.github.com>
2019-02-13 22:46:04 -0600

    Update 1.py
    Corrected to allow pasting of transaction log

--- Lab1/Source/1.py
+++ Lab1/Source/1.py
@@ -1,22 +1,31 @@
 # Initialize
-running = True
 balance = 0
 
 # Loop to get transactions
-while running:
-    transaction = input("Please enter your transaction:\n")
+lines = []
+print("Please paste your transaction log:\n")
+while True:
+    line = input()
+    if line == '':
+        break
+    else:
+        lines.append(line)
+
+for x in lines:
+
+    transaction = x
     current = transaction.split(' ')
 
     # If withdraw, remove amount from balance
     if current[0] == "Withdraw":
-        balance -= int(current[1])
+        balance -= float(current[1])
     # If deposit, add amount to balance
     elif current[0] == "Deposit":
-        balance += int(current[1])
+        balance += float(current[1])
 
-    answer = input("Would you like to continue making transactions?\n")
-    # If user answers no, quit looping
-    if answer[0].lower() == 'n':
-        running = False
+    # answer = input("Would you like to continue making transactions?\n")
+    # # If user answers no, quit looping
+    # if answer[0].lower() == 'n':
+    #     running = False
 
-print("Total Balance: $", balance)
+print("Total Balance: $%.2f" % balance)
