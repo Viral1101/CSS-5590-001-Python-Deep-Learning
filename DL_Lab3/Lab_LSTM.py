a5f2f0c9b6d2a2ec8669abf7759fac349ec6b1d0
Viral1101 <Viral1101@users.noreply.github.com>
2019-04-30 00:59:20 -0500

    initial commit

---
+++ DL_Lab3/Lab_LSTM.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import CountVectorizer
+from keras.callbacks import TensorBoard
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from sklearn.model_selection import train_test_split
+from keras.utils.np_utils import to_categorical
+import re
+
+# Initialize settings for TensorBoard
+tbCallBack = TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
+
+# Set random seed for reproducibility
+seed = 42
+np.random.seed(seed)
+
+train = pd.read_csv('./train.tsv', delimiter='\t', encoding='utf-8')
+test = pd.read_csv('./test.tsv', delimiter='\t', encoding='utf-8')
+
+train = train[['Phrase', 'Sentiment']]
+test = test[['Phrase']]
+
+max_features = 2000
+tokenizer = Tokenizer(num_words=max_features, split=' ')
+tokenizer2 = Tokenizer(num_words=max_features, split=' ')
+
+tokenizer.fit_on_texts(train['Phrase'].values)
+tokenizer2.fit_on_texts(test['Phrase'].values)
+
+X = tokenizer.texts_to_sequences(train['Phrase'].values)
+X = pad_sequences(X)
+
+X2 = tokenizer2.texts_to_sequences(test['Phrase'].values)
+X2 = pad_sequences(X2, maxlen=len(X[0]))
+
+print(len(X[0]))
+print(len(X2[0]))
+
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(train['Sentiment'])
+y_train = to_categorical(integer_encoded)
+
+# y_train = np_utils.to_categorical(train['Sentiment'])
+num_classes = y_train.shape[1]
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, y_train, test_size=0.33, random_state=seed)
+
+epochs = 10
+batch = 30
+embed_dim = 128
+lstm_out = 196
+
+
+def create_model():
+    model = Sequential()
+    model.add(Embedding(max_features, embed_dim,input_length = X.shape[1]))
+    model.add(SpatialDropout1D(0.4))
+    model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(5,activation='softmax'))
+    model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
+    return model
+
+
+# model.fit(X_train, Y_train, epochs = epochs, batch_size=batch, verbose = 1)
+# score,acc = model.evaluate(X_test,Y_test,verbose=2,batch_size=batch)
+# print(score)
+# print(acc)
+
+# model.save('./model2' + '.h5')
+
+model2 = KerasClassifier(build_fn=create_model)
+epochs2 = [1, 2, 3]
+batch2 = [10, 20, 30]
+param_grid = dict(batch_size=batch2, epochs=epochs2)
+from sklearn.model_selection import GridSearchCV
+grid = GridSearchCV(estimator=model2, param_grid=param_grid, n_jobs=-1)
+grid_result = grid.fit(X_train, Y_train)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
---
+++ DL_Lab3/Lab_NNs.py
@@ -0,0 +1,101 @@
+import numpy
+import keras
+import pandas as pd
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, Input
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.utils import np_utils
+from keras import backend as K
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils.np_utils import to_categorical
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from keras.wrappers.scikit_learn import KerasClassifier
+
+# Initialize settings for TensorBoard
+tbCallBack = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
+
+# Set random seed for reproducibility
+seed = 42
+numpy.random.seed(seed)
+
+train = pd.read_csv('./train.tsv', delimiter='\t', encoding='utf-8')
+test = pd.read_csv('./test.tsv', delimiter='\t', encoding='utf-8')
+
+train = train[['Phrase', 'Sentiment']]
+test = test[['Phrase']]
+
+max_features = 2000
+tokenizer = Tokenizer(num_words=max_features, split=' ')
+tokenizer2 = Tokenizer(num_words=max_features, split=' ')
+
+tokenizer.fit_on_texts(train['Phrase'].values)
+tokenizer2.fit_on_texts(test['Phrase'].values)
+
+X = tokenizer.texts_to_sequences(train['Phrase'].values)
+X = pad_sequences(X)
+
+X2 = tokenizer2.texts_to_sequences(test['Phrase'].values)
+X2 = pad_sequences(X2, maxlen=len(X[0]))
+
+print(len(X[0]))
+print(len(X2[0]))
+
+labelencoder = LabelEncoder()
+integer_encoded = labelencoder.fit_transform(train['Sentiment'])
+y_train = to_categorical(integer_encoded)
+
+# y_train = np_utils.to_categorical(train['Sentiment'])
+num_classes = y_train.shape[1]
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, y_train, test_size=0.33, random_state=seed)
+
+embed_dim = 128
+
+print(X_train.shape[1])
+
+epochs = 10
+batch = 30
+lrate = 0.01
+decay = lrate/epochs
+
+def create_model():
+    sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+    model = Sequential()
+    model.add(Embedding(max_features, embed_dim, input_length=X_train.shape[1]))
+    model.add(Conv1D(128, kernel_size=5, activation='relu', kernel_constraint=maxnorm(3)))
+    model.add(MaxPooling1D())
+    model.add(Conv1D(128, kernel_size=5, activation='relu', kernel_constraint=maxnorm(3)))
+    model.add(MaxPooling1D())
+    model.add(Flatten())
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    return model
+
+
+model = create_model()
+
+#
+# model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=epochs, batch_size=batch, callbacks=[tbCallBack])
+# # Final evaluation of the model
+# scores = model.evaluate(X_test, Y_test, verbose=1)
+# print("Accuracy: %.2f%%" % (scores[1]*100))
+# model.save('./model' + '.h5')
+
+model2 = KerasClassifier(build_fn=create_model)
+
+epochs2 = [1, 2, 3]
+batch2 = [10, 20, 30]
+param_grid = dict(batch_size=batch2, epochs=epochs2)
+from sklearn.model_selection import GridSearchCV
+grid = GridSearchCV(estimator=model2, param_grid=param_grid, n_jobs=-1)
+grid_result = grid.fit(X_train, Y_train)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
